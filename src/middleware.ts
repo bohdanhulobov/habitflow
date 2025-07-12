b959f592cf8cb6a1a532325f53a3e161e88412c8
Bohdan Hulobov <89200310+bohdanhulobov@users.noreply.github.com>
2025-07-12 11:29:37 +0300

    feat: migrate authentication middleware to src directory and update signOut page handling

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ token }) => {
+      return !!token;
+    },
+  },
+  pages: {
+    signIn: "/",
+    signOut: "/",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export const config = {
+  matcher: ["/dashboard", "/statistics"],
+};
